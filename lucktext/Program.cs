d626638530b77174e0cf80e91f56ede625f275e9
Egil Vilcans <egil.vilcans@gmail.com>
2022-08-31 14:22:29 +0200

    Initial commit

---
+++ lucktext/Program.cs
@@ -0,0 +1,31 @@
+﻿string name = "";
+string language = "";
+string day = "";
+
+EnterName:
+Console.WriteLine("Please enter your name:");
+name = Console.ReadLine();
+if (name == "")
+{
+    goto EnterName;
+}
+
+EnterLanguage:
+Console.WriteLine($"hello, {name}.\nWhat's your favourite programming language?");
+language = Console.ReadLine();
+if (language == "")
+{
+    goto EnterLanguage;
+}
+
+EnterDay:
+Console.WriteLine("What day is it?");
+day = Console.ReadLine();
+if (day == "")
+{
+    goto EnterDay;
+}
+
+Console.WriteLine($"On {day}, {name} said that their favourite programming language is {language}.");
+Console.WriteLine("\nPress Enter to end program");
+Console.ReadLine();
